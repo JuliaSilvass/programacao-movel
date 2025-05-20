0cba8b004e23b99cd9b74149eb2c58b24011b337
JuliaSilvass <juliasilvass01@gmail.com>
2025-05-19 21:57:09 -0300

    barra de navegação

--- financial-mobile/App.js
+++ financial-mobile/App.js
@@ -1,12 +1,50 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-export default function App() {
+const Drawer = createDrawerNavigator();
+
+//Tela Inicial
+function Inicial({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working n your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Tela da Início</Text>
     </View>
+  );
+}
+
+export default function App() {
+  const [colorIcone, setColorIcone] = useState("red");
+
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName="Tela Inicial"
+        screenOptions={{
+          drawerStyle: {
+            backgroundColor: "green",
+            width: "60%"
+          },
+          drawerActiveTintColor: "white",
+          drawerActiveBackgroundColor: "gray",
+          drawerInactiveTintColor: "black",
+        }}>
+        <Drawer.Screen
+          name="Tela Inicial"
+          component={Inicial}
+          options={{
+            drawerLabel: "Página do Começo",
+            headerShown: true,   // Ocultar titulo da página : false
+            drawerIcon: ({ colorIcone }) => <Icon name="home" size={20} color={colorIcone} />,
+            headerTintColor: "red",
+            headerStyle: { backgroundColor: "black" }
+          }}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
